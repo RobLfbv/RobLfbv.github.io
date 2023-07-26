9413fc6ca178e1da50782ecce770468f83bbad93
Robin Lefebvre <robin.lefebvre12@gmail.com>
2023-07-26 10:41:45 +0200

    update on development

--- ThreeJSScene/assets/js/main.js
+++ ThreeJSScene/assets/js/main.js
@@ -1,37 +1,38 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { createMaterialArray } from "./skyboxCreation.js";
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { Vector3 } from 'three';
-
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 /**
  * Variables de la scene
  */
 //Nom des differentes images pour la skybox dans les fichiers
-let skyboxImage = "nebula";
+let skyboxImage = "galaxy";
 //Nom des deux images pour la texture du cube
 let textureCubeTopBot = "./public/OakLog-topbot.png";
 let textureCubeSide = "./public/OakLog-side.png";
-let textureGrass = "./public/Grass.png";
+let textureGrass = "./public/Grass.jpg";
 //Vitesse de rotation de la camera
-let speedMovementInCircle = 0.002;
-
-
+let speedMovementInCircle = 0.001;
 
 /**
  * Initialisation de la scene, de la camera et de sa position et ajout de la scene à la page html
  */
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100000);
+const camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100000
+);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 //const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 let controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener('change', render);
+controls.addEventListener("change", render);
 function render() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 controls.minDistance = 0;
 controls.maxDistance = 800;
@@ -46,63 +47,64 @@ camera.rotation.set(-0.6, 0.3, 0.2);
 //const floorTexture = new THREE.TextureLoader().load('./public/checkerboard.jpg');
 const floorTexture = new THREE.TextureLoader().load(textureGrass);
 floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-floorTexture.repeat.set(100, 100);
-var floorMaterial = new THREE.MeshPhongMaterial({ map: floorTexture, side: THREE.DoubleSide });
-
+floorTexture.repeat.set(20, 20);
+var floorMaterial = new THREE.MeshPhongMaterial({
+  map: floorTexture,
+  side: THREE.DoubleSide,
+});
+floorMaterial.shininess = 0;
 const textureSideCube = new THREE.TextureLoader().load(textureCubeSide);
 const textureTopCube = new THREE.TextureLoader().load(textureCubeTopBot);
 const materialCube = [
-    new THREE.MeshPhongMaterial({ map: textureSideCube }),
-    new THREE.MeshPhongMaterial({ map: textureSideCube }),
-    new THREE.MeshPhongMaterial({ map: textureTopCube }),
-    new THREE.MeshPhongMaterial({ map: textureTopCube }),
-    new THREE.MeshPhongMaterial({ map: textureSideCube }),
-    new THREE.MeshPhongMaterial({ map: textureSideCube })];
+  new THREE.MeshPhongMaterial({ map: textureSideCube }),
+  new THREE.MeshPhongMaterial({ map: textureSideCube }),
+  new THREE.MeshPhongMaterial({ map: textureTopCube }),
+  new THREE.MeshPhongMaterial({ map: textureTopCube }),
+  new THREE.MeshPhongMaterial({ map: textureSideCube }),
+  new THREE.MeshPhongMaterial({ map: textureSideCube }),
+];
 const materialCube2 = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+const materialCircle = new THREE.MeshPhongMaterial({ color: 0x517832 });
 const materialSkybox = createMaterialArray(skyboxImage);
 
 /**
  * Creation de la lumiere
  */
-const light = new THREE.AmbientLight(0x404040, 0); // soft white light
+//const light = new THREE.AmbientLight(0x404040, 0); // soft white light
 const directionalLight = new THREE.DirectionalLight(0x404040, 0);
 directionalLight.position.set(1, 2, 0);
 
-const pointLight1 = new THREE.PointLight(0xffffff, 0.5, 500);
+const pointLight1 = new THREE.PointLight(0xffffff, 0.2, 1500);
 pointLight1.position.set(0, 100, 0);
 scene.add(pointLight1);
 
-const spotLight1 = new THREE.SpotLight(0xffffff, 0.6, 800, 30 * (Math.PI / 180));
+const spotLight1 = new THREE.SpotLight(0xaaaaff, 5, 500, 5 * (Math.PI / 180));
 spotLight1.position.set(-50, 50, 100);
 const targetObject1 = new THREE.Object3D();
-targetObject1.position.set(300, 0, -300);
+targetObject1.position.set(100, 0, -100);
 scene.add(targetObject1);
 spotLight1.target = targetObject1;
-scene.add(spotLight1);
 
-const spotLight2 = new THREE.SpotLight(0xffffff, 0.6, 800, 30 * (Math.PI / 180));
+const spotLight2 = new THREE.SpotLight(0xffaaff, 5, 500, 5 * (Math.PI / 180));
 spotLight2.position.set(50, 50, 100);
 const targetObject2 = new THREE.Object3D();
-targetObject2.position.set(-300, 0, -300);
+targetObject2.position.set(-100, 0, -100);
 scene.add(targetObject2);
 spotLight2.target = targetObject2;
-scene.add(spotLight2);
 
-const spotLight3 = new THREE.SpotLight(0xffffff, 0.6, 800, 30 * (Math.PI / 180));
+const spotLight3 = new THREE.SpotLight(0xffffaa, 5, 500, 5 * (Math.PI / 180));
 spotLight3.position.set(-50, 50, -100);
 const targetObject3 = new THREE.Object3D();
-targetObject3.position.set(300, 0, 300);
+targetObject3.position.set(100, 0, 100);
 scene.add(targetObject3);
 spotLight3.target = targetObject3;
-scene.add(spotLight3);
 
-const spotLight4 = new THREE.SpotLight(0xffffff, 0.6, 800, 30 * (Math.PI / 180));
+const spotLight4 = new THREE.SpotLight(0xffaaaa, 5, 500, 5 * (Math.PI / 180));
 spotLight4.position.set(50, 50, -100);
 const targetObject4 = new THREE.Object3D();
-targetObject4.position.set(-300, 0, 300);
+targetObject4.position.set(-100, 0, 100);
 scene.add(targetObject4);
 spotLight4.target = targetObject4;
-scene.add(spotLight4);
 
 const spotLight5 = new THREE.SpotLight(0xffffff, 1, 800, 20 * (Math.PI / 180));
 spotLight5.position.set(0, 500, 0);
@@ -110,12 +112,14 @@ const targetObject5 = new THREE.Object3D();
 targetObject5.position.set(0, 0, 0);
 scene.add(targetObject5);
 spotLight5.target = targetObject5;
+
+scene.add(spotLight1);
+scene.add(spotLight2);
+scene.add(spotLight3);
+scene.add(spotLight4);
 scene.add(spotLight5);
 
 scene.add(directionalLight);
-scene.add(light);
-
-
 
 /**
  * Creation des différents objets 3D
@@ -131,6 +135,20 @@ const floorGeometry = new THREE.PlaneGeometry(10000, 10000, 10, 10);
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 floor.position.y -= 0;
 floor.rotateX(90 * (Math.PI / 180));
+var json = floor.toJSON();
+console.log(json);
+//downloadObjectAsJson(json, "test");
+function downloadObjectAsJson(exportObj, exportName) {
+  var dataStr =
+    "data:text/json;charset=utf-8," +
+    encodeURIComponent(JSON.stringify(exportObj));
+  var downloadAnchorNode = document.createElement("a");
+  downloadAnchorNode.setAttribute("href", dataStr);
+  downloadAnchorNode.setAttribute("download", exportName + ".json");
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
 
 //Skybox
 const skyBoxGeometry = new THREE.BoxGeometry(10001, 10001, 10001);
@@ -138,58 +156,47 @@ const skyBox = new THREE.Mesh(skyBoxGeometry, materialSkybox);
 
 const loader = new GLTFLoader();
 
-loader.load('./public/stade.gltf', function (gltf) {
+loader.load(
+  "./public/stade.gltf",
+  function (gltf) {
     gltf.scene.position.set(0, 0, 0);
-    scene.add(gltf.scene);
-
-}, undefined, function (error) {
-
+    //scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
     console.error(error);
-
-});
+  }
+);
 
 //Montains
-/*
-loader.load('./public/mountains.gltf', function (gltf) {
-    gltf.scene.position.set(0, -4000, 0);
-    gltf.scene.scale.x = 4500;
-    gltf.scene.scale.y = 8000;
-    gltf.scene.scale.z = 4500;
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error(error);
-});
-
-loader.load('./public/mountains.gltf', function (gltf) {
-    gltf.scene.position.set(0, -4000, 0);
-    gltf.scene.scale.x = 4500;
-    gltf.scene.scale.y = 8000;
-    gltf.scene.scale.z = 4500;
-    gltf.scene.rotation.y = 80 * (Math.PI / 180);
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error(error);
-});
-*/
-/*
-loader.load('./public/mountains2.gltf', function (gltf) {
-    gltf.scene.position.set(0, -0, 0);
-    gltf.scene.scale.x = 500;
-    gltf.scene.scale.y = 150;
-    gltf.scene.scale.z = 500;
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error(error);
-});*/
-loader.load('./public/mountains3.gltf', function (gltf) {
+loader.load(
+  "./public/mountains3.gltf",
+  function (gltf) {
     gltf.scene.position.set(0, -10, 0);
     gltf.scene.scale.x = 650;
     gltf.scene.scale.y = 200;
     gltf.scene.scale.z = 650;
     scene.add(gltf.scene);
-}, undefined, function (error) {
+  },
+  undefined,
+  function (error) {
     console.error(error);
-});
+  }
+);
+
+//Creations de buissons
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+for (let i = 0; i < 50; i++) {
+  const geometryCircle = new THREE.SphereGeometry(5);
+  const circle = new THREE.Mesh(geometryCircle, materialCircle);
+  circle.position.set(getRandomInt(-1000, 1000), 5, getRandomInt(-1000, 1000));
+  scene.add(circle);
+}
 /**
  * Ajout a la scene de tous les objets 3D
  */
@@ -198,30 +205,191 @@ scene.add(floor);
 //scene.add(cube1);
 //scene.add(cube2);
 
-
 addEventListener("mouseup", (event) => {
-    cameraMovement = true;
-    camera.position.y = 200;
-
+  cameraMovement = true;
+  camera.position.y = 200;
 });
 
 addEventListener("mousedown", (event) => {
-    cameraMovement = false;
-    camera.position.y = 200;
-
+  cameraMovement = false;
+  camera.position.y = 200;
 });
 
 var t = 0;
 var cameraMovement = true;
-function animate() {
-    requestAnimationFrame(animate);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    if (cameraMovement) {
-        camera.position.x = 600 * Math.cos(t) + 0;
-        camera.position.z = 600 * Math.sin(t) + 0;
-        camera.lookAt(cube1.position);
-        t += speedMovementInCircle;
+
+const fireworks = [];
+const clock = new THREE.Clock();
+
+function createFirework(
+  scene,
+  position,
+  color,
+  particleCount,
+  particleSize,
+  explosionRadius,
+  explosionDuration,
+  fadeOutDuration
+) {
+  // Variables pour stocker les particules du feu d'artifice
+  const particles = new THREE.Group();
+
+  // Création du matériau pour les particules
+  const particleMaterial = new THREE.PointsMaterial({
+    size: particleSize,
+    color: color,
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    depthTest: false,
+    map: createParticleTexture(), // Cette fonction crée un sprite de particule
+  });
+
+  // Création des particules
+  const particleGeometry = new THREE.BufferGeometry();
+  const particlePositions = new Float32Array(particleCount * 3);
+  const particleVelocities = [];
+
+  for (let i = 0; i < particleCount; i++) {
+    const vertex = new THREE.Vector3();
+    particlePositions.set([vertex.x, vertex.y, vertex.z], i * 3);
+    particleVelocities.push(
+      new THREE.Vector3(
+        (Math.random() - 0.5) * 50,
+        (Math.random() - 0.5) * 50,
+        (Math.random() - 0.5) * 50
+      )
+    );
+  }
+
+  particleGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(particlePositions, 3)
+  );
+
+  // Création des points pour les particules
+  const particleSystem = new THREE.Points(particleGeometry, particleMaterial);
+  particles.add(particleSystem);
+
+  // Position du feu d'artifice
+  particles.position.copy(position);
+  scene.add(particles);
+
+  // Animation du feu d'artifice
+  function updateFirework(
+    particleSystem,
+    particleVelocities,
+    explosionDuration,
+    fadeOutDuration,
+    deltaTime
+  ) {
+    const particlePositions = particleSystem.geometry.attributes.position;
+
+    for (let i = 0; i < particlePositions.count; i++) {
+      const velocity = particleVelocities[i];
+      const position = particlePositions.array;
+      position[i * 3] += velocity.x * deltaTime;
+      position[i * 3 + 1] += velocity.y * deltaTime;
+      position[i * 3 + 2] += velocity.z * deltaTime;
+
+      // Appliquer une gravité pour que les particules retombent après l'explosion
+      velocity.y -= 9.8 * deltaTime;
     }
-    renderer.render(scene, camera);
+
+    if (particleSystem.userData.explosionTime < explosionDuration) {
+      particleSystem.userData.explosionTime += deltaTime;
+    } else {
+      particleSystem.material.opacity -= deltaTime / fadeOutDuration;
+      if (particleSystem.material.opacity <= 0) {
+        particleSystem.parent.remove(particleSystem);
+      }
+    }
+
+    particlePositions.needsUpdate = true;
+  }
+  // Animation loop
+  clock = new THREE.Clock();
+  const animate = () => {
+    updateFirework();
+    if (particles.children.length === 0) {
+      // Suppression de l'animation si le feu d'artifice a disparu
+      renderer.setAnimationLoop(null);
+    }
+  };
+
+  renderer.setAnimationLoop(animate);
+}
+
+function createParticleTexture() {
+  // Créez ici une texture pour les particules (un simple cercle ou étoile blanche sur fond transparent).
+  // Vous pouvez utiliser THREE.TextureLoader() pour charger une image externe ou créer une texture
+  // directement en dessinant sur un canvas.
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  const deltaTime = clock.getDelta();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  if (cameraMovement) {
+    camera.position.x = 600 * Math.cos(t) + 0;
+    camera.position.z = 600 * Math.sin(t) + 0;
+    camera.lookAt(cube1.position);
+    t += speedMovementInCircle;
+  }
+
+  if (Math.random() < 0.01) {
+    // Vous pouvez ajuster le taux d'apparition des feux d'artifice ici
+    const fireworkPosition = new THREE.Vector3(
+      (Math.random() - 0.5) * 200, // Position aléatoire en X dans une plage de -100 à 100
+      Math.random() * 200, // Position aléatoire en Y dans une plage de 0 à 200 (pour qu'il apparaisse au-dessus du sol)
+      (Math.random() - 0.5) * 200 // Position aléatoire en Z dans une plage de -100 à 100
+    );
+
+    const fireworkColor = new THREE.Color(
+      Math.random(), // Valeur aléatoire pour le canal rouge
+      Math.random(), // Valeur aléatoire pour le canal vert
+      Math.random() // Valeur aléatoire pour le canal bleu
+    );
+
+    const particleCount = 1000;
+    const particleSize = 2;
+    const explosionRadius = 100;
+    const explosionDuration = 2;
+    const fadeOutDuration = 1;
+
+    createFirework(
+      scene,
+      fireworkPosition,
+      fireworkColor,
+      particleCount,
+      particleSize,
+      explosionRadius,
+      explosionDuration,
+      fadeOutDuration
+    );
+    fireworks.push({
+      particleSystem,
+      particleVelocities: particleSystem.geometry.attributes.position.array.map(
+        (_, index) => {
+          return new THREE.Vector3(
+            (Math.random() - 0.5) * 5,
+            (Math.random() - 0.5) * 5,
+            (Math.random() - 0.5) * 5
+          );
+        }
+      ),
+      explosionDuration,
+      fadeOutDuration,
+    });
+  }
+  for (const firework of fireworks) {
+    updateFirework(
+      firework.particleSystem,
+      firework.particleVelocities,
+      firework.explosionDuration,
+      firework.fadeOutDuration,
+      deltaTime
+    );
+  }
+  renderer.render(scene, camera);
 }
 animate();
--- ThreeJSScene/assets/js/skyboxCreation.js
+++ ThreeJSScene/assets/js/skyboxCreation.js
@@ -1,36 +1,51 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 /**
- * 
- * @param {String} filename - Nom des differentes images qui composent la skybox  
+ *
+ * @param {String} filename - Nom des differentes images qui composent la skybox
  * @return {String[]} - Une liste de tous les noms des images
  */
 function createPathStrings(filename) {
-    const basePath = "./public/";
-    const baseFilename = basePath + filename;
-    const fileType = ".png";
-    const sides = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
-    const pathStings = sides.map(side => {
-        return baseFilename + "-" + side + fileType;
-    });
+  const basePath = "./public/";
+  const baseFilename = basePath + filename;
+  const fileType = ".png";
+  //const side = ["xpos", "xneg", "ypos", "yneg", "zneg", "zpos"];
+  //const side = ["1", "2", "5", "6", "3", "4"];
+  const side = ["1", "3", "5", "6", "2", "4"];
+  /*const pathStings = side.map(side => {
+    return baseFilename + "-" + side + fileType;
+  });*/
+  const pathStings = side.map((side) => {
+    return basePath + side + fileType;
+  });
 
-    return pathStings;
+  return pathStings;
 }
 /**
- * 
- * @param {*} filename - Nom des differentes images qui composent la skybox  
+ *
+ * @param {*} filename - Nom des differentes images qui composent la skybox
  * @returns - Une liste de toutes les textures de la skybox
  */
 export function createMaterialArray(filename) {
-    const skyboxImagepaths = createPathStrings(filename);
-    var materialArray = [];
-    for (let i = 0; i < 6; i++) {
-        let currentTexture = new THREE.TextureLoader().load(skyboxImagepaths[i]);
-        currentTexture.side = THREE.BackSide;
-        materialArray.push(new THREE.MeshBasicMaterial({ map: currentTexture }));
-    }
-    for (let i = 0; i < 6; i++)
-        materialArray[i].side = THREE.BackSide;
+  const skyboxImagepaths = createPathStrings(filename);
+  /*const skyboxImagepaths = [];
+  for (let i = 0; i < 6; i++) {
+    skyboxImagepaths[i] = "./public/" + filename + ".jpg";
+  }*/
+  console.log(skyboxImagepaths);
+  var materialArray = [];
+  for (let i = 0; i < 6; i++) {
+    let currentTexture = new THREE.TextureLoader().load(skyboxImagepaths[i]);
+    currentTexture.side = THREE.BackSide;
+    materialArray.push(
+      new THREE.MeshBasicMaterial({
+        map: currentTexture,
+        reflectivity: 0,
+        color: 0x909090,
+      })
+    );
+  }
+  for (let i = 0; i < 6; i++) materialArray[i].side = THREE.BackSide;
 
-    return materialArray;
+  return materialArray;
 }
